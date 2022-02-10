6dc9b041ae33933261103c1b9091cf49cccbaa25
Ganesh Patil <ganeshp2103@gmail.com>
2022-02-10 17:31:17 +0530

    Removed Animation for smooth scrolling

--- js/script.js
+++ js/script.js
@@ -19,23 +19,6 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 });
 
 
-// to use wow in html class - to enable animation only when element is loaded on screen  
-var wow = new WOW(
-    {
-        boxClass: 'wow',      // animated element css class (default is wow)
-        animateClass: 'animated', // animation css class (default is animated)
-        offset: 0,          // distance to the element when triggering the animation (default is 0)
-        mobile: true,       // trigger animations on mobile devices (default is true)
-        live: true,       // act on asynchronously loaded content (default is true)
-        callback: function (box) {
-            // the callback is fired every time an animation is started
-            // the argument that is passed in is the DOM node being animated
-        },
-        scrollContainer: null // optional scroll container selector, otherwise use window
-    }
-);
-wow.init();
-
 // Lode more - certifications
 function loadMore() {
 
